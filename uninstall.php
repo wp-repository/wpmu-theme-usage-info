8b3b40d9c53105e1e54a56a3f05994df19a7cf52
Christian Foellmann <foellmann@foe-services.de>
2015-01-15 19:39:00 +0100

    uninstall save for later

--- uninstall.php
+++ uninstall.php
@@ -1,16 +1,13 @@
 <?php
 /**
- * @author WP-Cloud <code@wp-cloud.de>
- * @license GPLv2 <http://www.gnu.org/licenses/gpl-2.0.html>
- * @package WPMU Theme Usage Info
+ * @author    Christian Foellmann & Jason Lemahieu and Kevin Graeme (Cooperative Extension Technology Services)
+ * @copyright Copyright (c) 2009 - 2014, Cooperative Extension Technology Services
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GPLv2
+ * @package   WP-Repository\WPMU_Theme_Usage_Info
  */
 
-//avoid direct calls to this file
-if ( !defined( 'ABSPATH' ) ) {
-	header( 'Status: 403 Forbidden' );
-	header( 'HTTP/1.1 403 Forbidden' );
-	exit();
-}
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 delete_site_option( 'cets_theme_info_version' );
 delete_site_option( 'cets_theme_info_data_freshness' );
